3d12da5d9e139fafd2ba8286e7caa1a1c4dda122
amondar <zaxodu11@gmail.com>
2025-11-02 06:38:15 +0300

    Update Pest configuration to use Unit tests, add `benchmark` method to TestCase class for measuring execution time

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,23 @@ declare(strict_types = 1);
 
 namespace Tests;
 
+use Closure;
+use JetBrains\PhpStorm\NoReturn;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    #[NoReturn]
+    final public function benchmark(Closure $callback): never
+    {
+        $start = hrtime(true);
+
+        $callback();
+
+        $elapsed = (hrtime(true) - $start) / 1e6;
+
+        fwrite(STDOUT, sprintf("time=%.3f ms\n", $elapsed));
+
+        exit(0);
+    }
 }
